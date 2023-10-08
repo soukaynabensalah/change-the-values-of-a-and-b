2b204a757a704bb656ca40cebbc863cc7638847d
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-10-08 21:25:46 +0100

    Create Values.py
    Echange Cond

---
+++ Values.py
@@ -0,0 +1,17 @@
+a=int(input("entrer un nbr a :"))
+b=int(input("entrer un nbr b :"))
+     if a*b>0 :
+       c=a
+       a=b
+       b=c
+     else
+       s=a+b
+       p=a*b
+       a=s
+       b=p
+print("a=",a)
+print("b=",b)
+
+       
+
+          
